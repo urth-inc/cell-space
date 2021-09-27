aeaae5681fb63bbbb9f32836eb55fc40a9b306e6
akriaueno <akiraueno@outlook.com>
2021-09-28 00:46:14 +0900

    rm test message

--- src/generate.py
+++ src/generate.py
@@ -62,7 +62,6 @@ def _tweet(population, seed, new_v, cur_p):
             f"抽出結果 : {new_v:02}番\n"
             f"今回の生成結果 : {cur_p:.1%}"
         )
-    tweet_str = "(テストです。)\n" + tweet_str
     access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
     access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
     api_key = os.environ.get("TWITTER_API_KEY")
